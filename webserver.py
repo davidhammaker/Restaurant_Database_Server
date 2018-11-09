e3503de236c1868832a483c170629f37777921b4
David J. Hammaker <davidjosh93@gmail.com>
2018-11-09 15:31:41 -0500

    Add Python 3 server template
    Template is based on 'webserver_py2.py'. Template has been ported to
    Python 3.

---
+++ webserver.py
@@ -0,0 +1,54 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+
+
+class WebserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html; charset=utf-8')
+                self.end_headers()
+
+                output = ""
+
+                self.wfile.write(output.encode())
+                print(output)
+                return
+
+        except IOError:
+            self.send_error(404, "File Not Found: {}".format(self.path))
+
+    def do_POST(self):
+        try:
+            length = int(self.headers.get('content-length', 0))
+            data = self.rfile.read(length).decode()
+            message = parse_qs(data)["message"][0]
+
+            self.send_response(301)
+            self.send_header('content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+
+            output = ""
+
+            self.wfile.write(output.encode())
+            print(output)
+
+        except:
+            pass
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebserverHandler)
+        print("Web server running on port {}.".format(port))
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print("\nKeyboard interruption. Stopping web server.")
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    main()
